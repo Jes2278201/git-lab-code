98a18f0852156bf58bf811ccc7be4e24b1dd8ac8
Jes2278201 <Jes2278201@maricopa.edu>
2024-11-02 03:16:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+#CIS 256 PROGRESS CHECK
+
+Read the py file to find my answer for the last question in part A
+also, here's a sonic the hedgehog list for my third file
---
+++ response.py
@@ -0,0 +1,6 @@
+print("hello professor, the last assignment didn't include a"
+      +" print statement at the bottom so")
+print("one thing i'd tell future students to"+
+      " make sure to read the module readings,"
+      +" they help a lot more then they'll realize"
+      +" then just going to stack overlow 24/7")
